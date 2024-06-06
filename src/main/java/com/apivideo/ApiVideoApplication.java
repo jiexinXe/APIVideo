2178186482ca50ef73da6b62bca16fd83e8639f3
hongshi1 <96938380+hongshi1@users.noreply.github.com>
2024-06-06 16:02:35 +0800

    全局跨域访问配置

--- src/main/java/com/apivideo/ApiVideoApplication.java
+++ src/main/java/com/apivideo/ApiVideoApplication.java
@@ -3,7 +3,10 @@ package com.apivideo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
@@ -11,5 +14,20 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 public class ApiVideoApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiVideoApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
     }
 }
