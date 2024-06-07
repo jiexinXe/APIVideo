6e2f2b0368b238b8ad597341f180eb5443d621bb
jiexinXe <jiexinXe@163.com>
2024-06-07 11:00:43 +0800

    bug修复：日志序列化与拦截器冲突问题

--- src/main/java/com/apivideo/log/LoggingAspect.java
+++ src/main/java/com/apivideo/log/LoggingAspect.java
@@ -7,6 +7,10 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Aspect
@@ -32,8 +36,13 @@ public class LoggingAspect {
 
         // 记录方法调用和参数
         String methodName = joinPoint.getSignature().toShortString();
-        String args = objectMapper.writeValueAsString(joinPoint.getArgs());
-        logger.info("Method {} called with arguments: {}", methodName, args);
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        String requestURI = request.getRequestURI();
+        String httpMethod = request.getMethod();
+        String clientIP = request.getRemoteAddr();
+
+        // 仅记录方法名、请求URL和HTTP方法
+        logger.info("Method {} called with URL: {}, HTTP Method: {}, Client IP: {}", methodName, requestURI, httpMethod, clientIP);
 
         Object result;
         try {
